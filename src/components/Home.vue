246c480d83e8fab784f051f12d9323a0d08aa6cb
Chems Eddine <chemsedd@outlook.fr>
2020-07-16 18:58:18 +0100

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <Profile />
+    <AboutMe />
+  </div>
+</template>
+
+<script>
+import Profile from "./AboutMe/Profile";
+import AboutMe from "./AboutMe/AboutMe";
+
+export default {
+  name: "Home",
+  components: {
+    Profile,
+    AboutMe
+  }
+};
+</script>
+
+<style>
+</style>
