8f4b85413627ddf4852326fac620b64b2a37142b
Chems Eddine Senoussi <chemsedd@outlook.fr>
2020-09-11 03:08:42 +0100

    ✌ List APPS from JSON file

--- src/components/Work/Program.vue
+++ src/components/Work/Program.vue
@@ -2,16 +2,12 @@
   <div class="card shadow-sm">
     <div class="card flex-row flex-wrap">
       <div class="card-header">
-        <img src="@/assets/logos/smart-green.jpg" style="width:200px" alt="Smart Green logo" />
+        <img :src="logopath" style="width:200px" alt="Smart Green logo" />
       </div>
       <div class="card-block p-4">
-        <h1 class="card-title font-weight-bold">Smart Green</h1>
-        <p class="card-text">Smart Green is a web platform for agricultural field monitoring.</p>
-        <a
-          href="https://github.com/chemsedd/smart_green"
-          target="_blank"
-          class="btn btn-primary"
-        >Github repo</a>
+        <h1 class="card-title font-weight-bold">{{name}}</h1>
+        <p class="card-text">{{description}}</p>
+        <a :href="gitrepohref" target="_blank" class="btn btn-primary">Github repo</a>
       </div>
     </div>
   </div>
@@ -20,6 +16,20 @@
 <script>
 export default {
   name: "Program",
+  props: {
+    name: {},
+    logo: {},
+    description: {},
+    gitrepo: {},
+  },
+  computed: {
+    logopath: function () {
+      return require(`@/assets/apps/${this.logo}.jpg`);
+    },
+    gitrepohref: function () {
+      return `https://github.com/chemsedd/${this.gitrepo}`;
+    },
+  },
 };
 </script>
 
--- src/components/Work/ProgramsRow.vue
+++ src/components/Work/ProgramsRow.vue
@@ -1,16 +1,30 @@
 <template>
   <div class="row p-4">
-    <div class="col-md-12 p-4">
-      <Program />
+    <div v-for="app in apps" :key="app.id" class="w-100">
+      <div class="col-md-12 p-4">
+        <Program
+          :name="app.name"
+          :logo="app.logo"
+          :description="app.description"
+          :gitrepo="app.gitrepo"
+        />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import Program from "@/components/Work/Program.vue";
+import AppsFile from "@/assets/apps/apps.json";
+
 export default {
   components: {
     Program,
+  },
+  data: function () {
+    return {
+      apps: AppsFile.apps,
+    };
   },
 };
 </script>
