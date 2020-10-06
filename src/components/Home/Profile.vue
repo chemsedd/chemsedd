03638352ac88578d0453bbad0b84b5953fc16b11
Chems Eddine Senoussi <chemsedd@outlook.fr>
2020-10-06 20:57:39 +0100

    Deleted README

--- src/components/Home/Profile.vue
+++ src/components/Home/Profile.vue
@@ -3,12 +3,15 @@
     <div class="col-md-4" align="center">
       <img class="rounded-circle w-75 p-1 peach-gradient" src="@/assets/me.jpg" />
     </div>
+    <div class="col-md-8">
+      <button class="btn aqua-gradient shadow-sm">Click me</button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Profile"
+  name: "Profile",
 };
 </script>
 
